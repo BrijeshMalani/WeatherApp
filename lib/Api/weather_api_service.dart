91de5951c9d138df00b51104d7fd20ef0bde490f
Brijesh Malani <brijesh.malani86@gmail.com>
2022-07-23 15:23:52 +0530

    newcode

--- lib/Api/weather_api_service.dart
+++ lib/Api/weather_api_service.dart
@@ -5,7 +5,7 @@ import 'package:weather_learn_api/Api/forcast_weather_api_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  Future<Forecast?> gigetcurrentWeather(String? location) async {
+  Future<Forecast?> getcurrentWeather(String? location) async {
     var response = await http.get(Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=2e9714911e1deb0a2ee62104c0b5928b&units=metric"));
     if (response.statusCode == 200) {
